b120b340873615d56f3337201088fc6ac3675048
Per Pasgaard <pasgaard@gmail.com>
2017-07-02 10:43:17 +0200

    Initial commit

---
+++ WPFDatabaseTest/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data.Entity;
+using System.Windows;
+
+namespace WPFDatabaseTest
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			CreateData();
+		}
+
+		private void CreateData()
+		{
+			using (var db = new AnimalContext())
+			{
+				Animal animal = new Animal { Name = "Big", LastName = "Elephant", Created = DateTime.Now, Modified = DateTime.Now };
+
+				db.Animals.Add(animal);
+				db.SaveChanges();
+
+				foreach (var item in db.Animals)
+					Console.WriteLine($"{item.AnimalId}  {item.Name} {item.LastName}");
+
+				ToString();
+			}
+		}
+	}
+
+	public class AnimalContext : DbContext
+	{
+		public AnimalContext() : base()
+		{
+			
+		}
+
+		public DbSet<Animal> Animals { get; set; }
+	}
+
+	public class Animal
+	{
+		public int AnimalId { get; set; }
+		public string Name { get; set; }
+		public string LastName { get; set; }
+		public DateTime Created { get; set; }
+		public DateTime Modified { get; set; }
+	}
+
+}
